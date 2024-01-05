803ff42b7e9f9b16c61ad90770e0434dd7dae4d5
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-01-05 10:37:54 +0700

    ...

--- android/lengthconvert/app/src/main/java/com/example/lengthconvert/MainActivity.kt
+++ android/lengthconvert/app/src/main/java/com/example/lengthconvert/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.lengthconvert
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    val btn=findViewById<Button>(R.id.button)
+    val rCm=findViewById<RadioButton>(R.id.rCm)
+    val rInch= findViewById<RadioButton>(R.id.rInch)
+    val input= findViewById<EditText>(R.id.tInput)
+    val output=findViewById<TextView>(R.id.tOutput)
+    rCm.isChecked=true
+    btn.setOnClickListener {
+ val factor=if(rCm.isChecked) 1/2.54 else 2.54
+ output.text="%.3f".format(input.text.toString().toDouble() *factor)+
+         if(rCm.isChecked) resources.getString(R.string.inch) else
+             resources.getString(R.string.cm)
+    }
     }
 }
