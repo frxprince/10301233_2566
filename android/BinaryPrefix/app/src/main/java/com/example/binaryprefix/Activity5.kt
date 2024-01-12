0875d67f2bc749204b12e38aabec2bbd255216b5
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-01-12 09:57:40 +0700

    ...

--- android/BinaryPrefix/app/src/main/java/com/example/binaryprefix/Activity5.kt
+++ android/BinaryPrefix/app/src/main/java/com/example/binaryprefix/Activity5.kt
@@ -2,10 +2,13 @@ package com.example.binaryprefix
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class Activity5 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_5)
+    val txtoutput=findViewById<TextView>(R.id.tOutput)
+    txtoutput.text=intent.getStringExtra("data")
     }
 }
