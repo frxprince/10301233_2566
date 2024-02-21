f6064c693dfaebaee93b15d8a3bd0c0b8b74d8c6
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-02-21 10:45:26 +0700

    ...

---
+++ android/SMSRemote/app/src/main/java/com/example/smsremote/MySMSReceiver.kt
@@ -0,0 +1,24 @@
+package com.example.smsremote
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.provider.Telephony
+import android.util.Log
+
+class MySMSReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+var message=Telephony.Sms.Intents.getMessagesFromIntent(intent)[0]
+Log.v("My remotecontroler",message.displayMessageBody)
+var i=Intent(context,MainActivity::class.java)
+i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+i.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+if(message.displayMessageBody=="cat"){
+  i.putExtra("data","cat");context.startActivity(i)
+}
+        if(message.displayMessageBody=="alarm"){
+            i.putExtra("data","alarm");context.startActivity(i)
+        }
+    }
+}
