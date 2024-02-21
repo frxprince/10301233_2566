3e9138d5d34353fa75b81d9ef817f14bd1578dea
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-02-21 10:56:10 +0700

    ...

--- android/SMSRemote/app/src/main/java/com/example/smsremote/MainActivity.kt
+++ android/SMSRemote/app/src/main/java/com/example/smsremote/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.smsremote
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    lateinit var Mp_alarm: MediaPlayer
+    lateinit var Mp_cat: MediaPlayer
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +18,15 @@ class MainActivity : AppCompatActivity() {
 if (checkSelfPermission(Manifest.permission.RECEIVE_SMS)!=PackageManager.PERMISSION_GRANTED)
  requestPermissions(arrayOf(Manifest.permission.RECEIVE_SMS),1234)
  btnExit.setOnClickListener { finish() }
+Mp_cat= MediaPlayer.create(this,R.raw.cat)
+Mp_alarm= MediaPlayer.create(this,R.raw.alarm)
+if(intent.getStringExtra("data")=="cat")Mp_cat.start()
+ if(intent.getStringExtra("data")=="alarm")Mp_alarm.start()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if(intent!!.getStringExtra("data")=="cat")Mp_cat.start()
+        if(intent!!.getStringExtra("data")=="alarm")Mp_alarm.start()
     }
 }
--- android/SMSRemote/app/src/main/java/com/example/smsremote/MySMSReceiver.kt
+++ android/SMSRemote/app/src/main/java/com/example/smsremote/MySMSReceiver.kt
@@ -3,6 +3,7 @@ package com.example.smsremote
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.MediaPlayer
 import android.provider.Telephony
 import android.util.Log
 
