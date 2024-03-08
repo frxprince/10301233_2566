1d66c85a492dc1b142e8ad6a18ae17c238173fc5
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-03-08 09:33:27 +0700

    ..

--- android/mylocation/app/src/main/java/com/example/mylocation/MainActivity.kt
+++ android/mylocation/app/src/main/java/com/example/mylocation/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.mylocation
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
+  lateinit var txtLocation:TextView
+  lateinit var txtNMEA:TextView
+  lateinit var btnClear: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+ txtNMEA=findViewById(R.id.tNMEA);txtLocation=findViewById(R.id.tLocation);btnClear=findViewById(R.id.bClear)
+if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
+  requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
+      Manifest.permission.ACCESS_COARSE_LOCATION),1234)
     }
 }
