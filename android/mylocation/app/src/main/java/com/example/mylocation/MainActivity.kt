7f67fa2c38f85463e5efcfbc560e0afa4afdc8e8
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-03-08 10:01:05 +0700

    ..

--- android/mylocation/app/src/main/java/com/example/mylocation/MainActivity.kt
+++ android/mylocation/app/src/main/java/com/example/mylocation/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.location.OnNmeaMessageListener
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,7 +13,7 @@ import android.widget.TextView
 import androidx.core.content.getSystemService
 import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity(),LocationListener {
+class MainActivity : AppCompatActivity(),LocationListener,OnNmeaMessageListener {
   lateinit var txtLocation:TextView
   lateinit var txtNMEA:TextView
   lateinit var btnClear: Button
@@ -25,6 +26,10 @@ if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManage
   requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,
       Manifest.permission.ACCESS_COARSE_LOCATION),1234)
   GPS=getSystemService(LOCATION_SERVICE) as LocationManager
+  GPS.addNmeaListener(this)
+    }
+    override fun onNmeaMessage(message: String?, timestamp: Long) {
+     txtNMEA.text="$timestamp:$message\n" + txtNMEA.text.toString();if(txtNMEA.text.length>2000)txtNMEA.text=""
     }
     override fun onResume() { super.onResume();
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED)
@@ -34,4 +39,6 @@ if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManage
 txtLocation.text="Lat:${location.longitude}\nLong:${location.longitude}\nAlt:${location.altitude}"+
         "\nTime:${location.time}\nSpeed:${location.speed}\nBearing:${location.bearing}"
     }
+
+
 }
