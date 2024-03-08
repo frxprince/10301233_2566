f922d8412401802f878ed4bf72b213931bc07425
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-03-08 11:21:25 +0700

    ..

--- android/PrimeNumber/app/src/main/java/com/example/primenumber/MainActivity.kt
+++ android/PrimeNumber/app/src/main/java/com/example/primenumber/MainActivity.kt
@@ -6,6 +6,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.TextView
+import kotlin.math.round
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnStart:Button
@@ -19,24 +20,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     btnStart=findViewById(R.id.bStart);btnCancel=findViewById(R.id.bCancel);txtInput=findViewById(R.id.eInput)
     txtOutput=findViewById(R.id.tOutput);progressbar=findViewById(R.id.progressBar)
-    var worker:PrimeFinder
+    lateinit var worker:PrimeFinder
+    progressbar.max=100
     btnStart.setOnClickListener {
 //txtOutput.text=primeK(txtInput.text.toString().toLong()).toString()
  worker=object:PrimeFinder(txtInput.text.toString().toLong()){
      override fun ShowOutput(k: Long) {
       txtOutput.text=k.toString()
      }
+
+     override fun ShowProgress(progress: Int) {
+     this@MainActivity.runOnUiThread { progressbar.progress=progress}
+     }
  }
   worker.start()
     }
+    btnCancel.setOnClickListener { worker.Cancel() }
     }
 
     abstract class PrimeFinder(var k:Long):Thread(){
+var Running=true
         override fun run() {
             super.run()
+            Running=true
          var ans=primeK(k) ;  ShowOutput(ans)
         }
+        fun Cancel(){
+         Running=false
+        }
         abstract fun ShowOutput(k:Long)
+        abstract fun ShowProgress(progress:Int)
         fun isPrime(x:Long):Boolean{
 
             for(i in 2..x-1)
@@ -47,10 +60,11 @@ class MainActivity : AppCompatActivity() {
         fun primeK(x:Long):Long{
             var i=3L
             var c=0L
-            while(true){
+            while(Running){
                 if(isPrime(i))
                 {
                     c++
+        ShowProgress(round((c/x.toFloat())*100).toInt())
                     if(c==x)
                         return i
                 }
