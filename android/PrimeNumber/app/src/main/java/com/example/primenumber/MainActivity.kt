7713258e8f93a9fcecdbacb937c54ff6b0ab97a9
Dr Paween Khoenkaw <paween_k@mju.ac.th>
2024-03-08 11:05:30 +0700

    ..

--- android/PrimeNumber/app/src/main/java/com/example/primenumber/MainActivity.kt
+++ android/PrimeNumber/app/src/main/java/com/example/primenumber/MainActivity.kt
@@ -19,8 +19,46 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     btnStart=findViewById(R.id.bStart);btnCancel=findViewById(R.id.bCancel);txtInput=findViewById(R.id.eInput)
     txtOutput=findViewById(R.id.tOutput);progressbar=findViewById(R.id.progressBar)
+    var worker:PrimeFinder
     btnStart.setOnClickListener {
-        
+//txtOutput.text=primeK(txtInput.text.toString().toLong()).toString()
+ worker=object:PrimeFinder(txtInput.text.toString().toLong()){
+     override fun ShowOutput(k: Long) {
+      txtOutput.text=k.toString()
+     }
+ }
+  worker.start()
     }
     }
+
+    abstract class PrimeFinder(var k:Long):Thread(){
+        override fun run() {
+            super.run()
+         var ans=primeK(k) ;  ShowOutput(ans)
+        }
+        abstract fun ShowOutput(k:Long)
+        fun isPrime(x:Long):Boolean{
+
+            for(i in 2..x-1)
+                if ((x%i) == 0L)
+                    return false
+            return true
+        }
+        fun primeK(x:Long):Long{
+            var i=3L
+            var c=0L
+            while(true){
+                if(isPrime(i))
+                {
+                    c++
+                    if(c==x)
+                        return i
+                }
+                i++
+            }
+            return 0
+        }
+    }
+
+
 }
